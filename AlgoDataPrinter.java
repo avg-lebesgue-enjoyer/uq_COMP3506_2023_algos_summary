bdcf477828ade453a12532bbb251dc95f464f4cc
avg-lebesgue-enjoyer <gabriel.field@uqconnect.edu.au>
2023-08-18 07:06:47 +1000

    Printer planned

---
+++ AlgoDataPrinter.java
@@ -0,0 +1,21 @@
+/**
+ * Static class for printing lists of algos/datas associated with a data/algo.
+ */
+public class AlgoDataPrinter {
+    /**
+     * Print algorithms associated to a given data structure.
+     * @param data DataTreeNode to print algos for
+     * @param pairing AlgoDataPairing which associates algos and data structures
+     * @return String human-readable prototype string.
+     */
+    public static String prototypePrintAlgos(DataTreeNode data, AlgoDataPairing pairing) {
+        throw new UnsupportedOperationException("haven't implemented yet!");
+    }
+
+    /* TODO:
+     *  - Print algorithms associated to a given data structure, in LaTeX.
+     *  - Print ^^, in *preamble* LaTeX.
+     *  - Print ^^, for all data structures, in *preamble* LaTeX.
+     *  - sim. Algos -> Datas
+     */
+}
